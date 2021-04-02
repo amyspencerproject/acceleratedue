4e343412c9852def74732ad41168cd34f1917adb
Amy Spencer <76791165+amyspencerproject@users.noreply.github.com>
2021-04-02 12:00:18 -0600

    Building About page up. Have some of the code needed in the about-page.php file. Want this commit to be starting point for trying to get it correct

--- wp-content/themes/accelerate-theme-child/custom-templates/about-page.php
+++ wp-content/themes/accelerate-theme-child/custom-templates/about-page.php
@@ -10,19 +10,29 @@
 
 get_header(); ?>
 
-	<div id="primary" class="about-page">
+<div id="primary" class="home-page hero-content">
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
+				<?php the_content(); ?>
+				<p>Accelerate is a strategy and marketing agency located in the heart of NYC. Our goal is to build businesses by making our clients visible and making their customers smile.</p>
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- .main-content -->
 
-            <h2>Our Services</h2>
+	<div id="primary" class="about-page">
+		<div class="main-content" role="main">
+			
+			<?php while ( have_posts() ) : the_post(); ?>
+
+
+			<h2>Our Services</h2>
 			<p>We take pride in our clients and the content we create for them. <br>Here's a brief overview of our offered services.</p>
 				<?php the_content(); ?>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 
-		<div id="contact" class="">
+		<div id="contact" class="about-contact">
 			<h1>Interested in working with us?</h1>
-			<p>Contact Us button<p>
+			<a class="button" href="<?php echo site_url('/contact-us/') ?>">Contact Us</a>
 		</div>
 
 	</div><!-- #primary -->
