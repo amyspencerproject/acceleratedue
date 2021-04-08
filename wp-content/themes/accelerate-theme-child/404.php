0af4206ccb599f19ddb85bde69d8c4ebaab0812a
Amy Spencer <76791165+amyspencerproject@users.noreply.github.com>
2021-04-07 18:13:00 -0600

    Created and styled a 404 Error page

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 404 Page
+ *
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+
+<div id="four-o-four-page"> 
+	<div class="main-content" role="main">
+        <div class="four-o-four-text">
+            <h1>404 Error<br>Exitinction happens at the most inopportune times!</h1>
+            <p>This page no longer exists. We cannot go backward in time but you can so please try again.</p>
+        </div>
+    </div><!-- .main-content -->
+</div><!-- #about-hero -->
+
+
+<?php get_footer (); ?>
